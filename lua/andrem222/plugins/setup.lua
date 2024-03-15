e069ac2bcda22d83d26af3c11d5fead3c42e009e
Andre Mossi <mossiroberto0392@gmail.com>
2024-03-15 11:29:54 -0400

    fix(offset encoding): fix offset encoding from clangd
    This was fixed by moving the capabilities changes from the folding to lspconfig

--- lua/andrem222/plugins/setup.lua
+++ lua/andrem222/plugins/setup.lua
@@ -52,6 +52,11 @@ return {
 
             local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
             capabilities.offsetEncoding = { "utf-16" }
+            capabilities.textDocument.foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true
+            }
+
 
             -- TypeScript
             nvim_lsp.tsserver.setup({
--- lua/andrem222/plugins/ui.lua
+++ lua/andrem222/plugins/ui.lua
@@ -160,19 +160,6 @@ return {
 
             ufo.setup()
 
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities.textDocument.foldingRange = {
-                dynamicRegistration = false,
-                lineFoldingOnly = true
-            }
-
-            local language_servers = require("lspconfig").util.available_servers()
-            for _, ls in ipairs(language_servers) do
-                require('lspconfig')[ls].setup({
-                    capabilities = capabilities
-                })
-            end
-
             vim.api.nvim_create_user_command(
                 "OpenAllFolds",
                 function()
